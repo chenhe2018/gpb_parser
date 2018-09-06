2859cdd0fe7093633fb6a232f82c3577cafec3e3
chenhe2018 <watch_ch@163.com>
2018-09-06 14:40:10 +0800

    build_release,build,cmake -DCMAKE_BUILD_TYPE=Release

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/etc/sh
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/protobuf/lib
+export LIBRARY_PATH=$LIBRARY_PATH:`pwd`/protobuf/lib
+
+export PATH=$PATH:`pwd`/protobuf/bin
+
+# 编译子文件夹中proto
+protoc -I=./message/ --cpp_out=./message/ ./message/*.proto
+
+mkdir build -p && cd build
+cmake .. && make && cd ..
+
+# main, with shared lib
+#g++ -o ./bin/main_bash ./test/main.cpp ./message/family.pb.cc -std=c++11 -Wall -g -I`pwd`/protobuf/include -L`pwd`/protobuf/lib -lprotobuf -I`pwd`/bin -L`pwd`/bin -lPbPath
+
+# test-efficiency
+#g++ -o ./bin/test-efficiency_bash ./test/test-efficiency.cpp ./message/testmsg.pb.cc -Wall -std=c++11 -I./protobuf/include -L./protobuf/lib -lprotobuf -I`pwd`/bin -L`pwd`/bin -lPbPath
--- clean.sh
+++ clean.sh
@@ -1,1 +1,1 @@
-rm -rf CMakeFiles/ cmake_install.cmake CMakeCache.txt Makefile ./message/*.pb.* libpbpath.so t bin/ pack/ CPack* build/ _CPack_Packages/ cmake-build-debug/*
+rm -rf CMakeFiles/ cmake_install.cmake CMakeCache.txt Makefile ./message/*.pb.* libpbpath.so t bin/ pack/ CPack* build/ _CPack_Packages/ ./cmake-build-debug/* build-release/
